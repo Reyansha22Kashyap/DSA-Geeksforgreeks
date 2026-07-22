7be17ec1d0517a41c9dae5bf33bae74c1580e31a
Reyansha Kashyap <221075540+Reyansha22Kashyap@users.noreply.github.com>
2026-07-22 18:01:29 +0530

    added MeetingRooms solution to calculate rooms

---
+++ Meeting rooms ||.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int minMeetingRooms(vector<int> &start, vector<int> &end) {
+        int n=start.size();
+        sort(start.begin(),start.end());
+        sort(end.begin(),end.end());
+        int i=0,j=0,room=0,res=0;
+        while(i<n && j<n){
+            if(start[i]<end[j]){
+                room++;
+                res=max(res,room);
+                i++;
+            }else{
+                room--;
+                j++;
+            }
+        }      
+              return res;
+    }
+};
