5e622d3623c313d47b7f371f02d14f6e1ee57a1a
Reyansha Kashyap <221075540+Reyansha22Kashyap@users.noreply.github.com>
2026-08-23 18:25:31 +0530

    Implement findCeil function in Solution class

---
+++ Ceil in a Sorted Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int findCeil(vector<int>& arr, int x) {
+        // code here
+        int n=arr.size();
+        int low=0,high=n-1;
+        int res=-1;
+        while(low<=high){
+            int guess=(low+high)/2;
+            if(arr[guess]<x)
+            low=guess+1;
+            else{
+                res=guess;
+                high=high-1;
+            }
+        } 
+              return res;
+    }
+};
